1ee3f55c2bbf5ed247c71b30fef9a2fa6630216a
saadzaoual <saadzaoual@gmail.com>
2025-06-13 17:40:33 +0100

    tokinazation to the operations

--- includes/parser.h
+++ includes/parser.h
@@ -12,7 +12,8 @@
 
 #ifndef PARSER_H
 # define PARSER_H
-
+#include <stdio.h>
 char **split_input(char *input);
+int has_unclosed_quotes(const char *str);
 
 #endif
--- src/parser/tokinizer.c
+++ src/parser/tokinizer.c
@@ -13,97 +13,132 @@
 #include "../includes/parser.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include <stdbool.h>
 
-static int count_words(const char *str);
-static char *extract_token(const char **str_ptr);
+static bool is_operator(char c)
+{
+    return (c == '>' || c == '<' || c == '|');
+}
+
+int has_unclosed_quotes(const char *str)
+{
+    char quote = 0;
+
+    while (*str)
+    {
+        if (!quote && (*str == '\'' || *str == '"'))
+            quote = *str;
+        else if (quote && *str == quote)
+            quote = 0;
+        str++;
+    }
+    return (quote != 0);
+}
+
+static char *get_token(const char **str_ptr)
+{
+    const char *s = *str_ptr;
+    char *token;
+    int len = 0;
+    char quote = 0;
+
+    while (*s == ' ')
+        s++;
+
+    // Handle operators
+    if (is_operator(*s))
+    {
+        if ((s[0] == '>' && s[1] == '>') || (s[0] == '<' && s[1] == '<'))
+            len = 2;
+        else
+            len = 1;
+
+        token = malloc(len + 1);
+        if (!token)
+            return NULL;
+        strncpy(token, s, len);
+        token[len] = '\0';
+        *str_ptr = s + len;
+        return token;
+    }
+
+    // Handle quoted strings
+    if (*s == '\'' || *s == '"')
+    {
+        quote = *s++;
+        const char *start = s;
+        while (*s && *s != quote)
+        {
+            s++;
+            len++;
+        }
+
+        token = malloc(len + 3); // +2 for quotes, +1 for \0
+        if (!token)
+            return NULL;
+        token[0] = quote;
+        strncpy(token + 1, start, len);
+        token[len + 1] = quote;
+        token[len + 2] = '\0';
+
+        if (*s == quote)
+            s++; // skip closing quote
+
+        *str_ptr = s;
+        return token;
+    }
+
+    // Handle normal words
+    const char *start = s;
+    while (*s && *s != ' ' && !is_operator(*s))
+    {
+        s++;
+        len++;
+    }
+
+    token = malloc(len + 1);
+    if (!token)
+        return NULL;
+    strncpy(token, start, len);
+    token[len] = '\0';
+
+    *str_ptr = s;
+    return token;
+}
 
 char **split_input(char *input)
 {
-    int word_count = count_words(input);
-    char **result = malloc(sizeof(char *) * (word_count + 1));
-    if (!result)
+    if (!input)
         return NULL;
 
-    int i = 0;
+    if (has_unclosed_quotes(input))
+    {
+        fprintf(stderr, "minishell: syntax error: unclosed quote\n");
+        return NULL;
+    }
+
+    int max_tokens = strlen(input); // worst case: one char per token
+    char **tokens = malloc(sizeof(char *) * (max_tokens + 1));
+    if (!tokens)
+        return NULL;
+
     const char *ptr = input;
+    int i = 0;
+
     while (*ptr)
     {
         while (*ptr == ' ')
             ptr++;
         if (*ptr)
-            result[i++] = extract_token(&ptr);
-    }
-    result[i] = NULL;
-    return result;
-}
-
-static int count_words(const char *str)
-{
-    int count = 0;
-    bool in_quote = false;
-    char quote_char = 0;
-
-    while (*str)
-    {
-        while (*str == ' ')
-            str++;
-        if (*str)
         {
-            count++;
-            while (*str && (in_quote || (*str != ' ')))
-            {
-                if (!in_quote && (*str == '\'' || *str == '\"'))
-                {
-                    in_quote = true;
-                    quote_char = *str;
-                }
-                else if (in_quote && *str == quote_char)
-                {
-                    in_quote = false;
-                }
-                str++;
-            }
+            tokens[i] = get_token(&ptr);
+            if (!tokens[i])
+                break;
+            i++;
         }
     }
-    return count;
-}
 
-static char *extract_token(const char **str_ptr)
-{
-    const char *str = *str_ptr;
-    char quote = 0;
-    int len = 0;
-
-    while (*str == ' ')
-        str++;
-
-    while (*str && (quote || *str != ' '))
-    {
-        if (!quote && (*str == '\'' || *str == '\"'))
-            quote = *str;
-        else if (quote && *str == quote)
-            quote = 0;
-        str++;
-        len++;
-    }
-
-    char *token = malloc(len + 1);
-    if (!token)
-        return NULL;
-
-    int j = 0;
-    quote = 0;
-    while (**str_ptr && (quote || **str_ptr != ' ') && j < len)
-    {
-        if (!quote && (**str_ptr == '\'' || **str_ptr == '\"'))
-            quote = **str_ptr;
-        else if (quote && **str_ptr == quote)
-            quote = 0;
-        else
-            token[j++] = **str_ptr;
-        (*str_ptr)++;
-    }
-    token[j] = '\0';
-    return token;
+    tokens[i] = NULL;
+    return tokens;
 }
