cbd75316e08035a1c61735ee456e5a0b76fed776
Swaggboysaad <saadzaoual@gmail.com>
2025-06-15 18:34:29 +0100

    cleaned files

--- src/parser/parcer.c
+++ src/parser/parcer.c
@@ -6,115 +6,122 @@
 /*   By: szaoual <szaoual@students.1337.ma>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/14 20:30:16 by szaoual           #+#    #+#             */
-/*   Updated: 2025/06/14 21:24:56 by szaoual          ###   ########.fr       */
+/*   Updated: 2025/06/15 18:32:36 by szaoual          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/parser.h"
 #include <stdlib.h>
-#include <string.h>
-#include "../../includes/parser.h"
+#include <unistd.h>
 
 #define MAX_CMDS 64
 #define MAX_TOKENS 256
-
-char ***split_by_pipe(char **tokens, int *cmd_count)
-{
-    char ***commands = malloc(sizeof(char **) * MAX_CMDS);
-    if (!commands)
-        return NULL;
-
-    int i = 0, j = 0, k = 0;
-
-    commands[k] = malloc(sizeof(char *) * MAX_TOKENS);
-    if (!commands[k])
-        return NULL;
-
-    while (tokens[i])
-    {
-        if (strcmp(tokens[i], "|") == 0)
-        {
-            commands[k][j] = NULL;
-            k++;
-            j = 0;
-            commands[k] = malloc(sizeof(char *) * MAX_TOKENS);
-            if (!commands[k])
-                return NULL;
-        }
-        else
-        {
-            commands[k][j++] = strdup(tokens[i]);
-        }
-        i++;
-    }
-
-    commands[k][j] = NULL;
-    *cmd_count = k + 1;
-    return commands;
-}
-
 #define MAX_ARGS 64
 #define MAX_REDIRECTS 1024
 
-t_cmd *parse_tokens(char **tokens)
+static int ft_strcmp(const char *s1, const char *s2)
 {
-    t_cmd *cmd = calloc(1, sizeof(t_cmd));
-    if (!cmd)
+    while (*s1 && *s1 == *s2)
+        s1++, s2++;
+    return (*(unsigned char *)s1 - *(unsigned char *)s2);
+}
+
+static char *ft_strdup(const char *s)
+{
+    int len = 0;
+    while (s[len])
+        len++;
+    char *dup = malloc(len + 1);
+    if (!dup)
         return NULL;
+    for (int i = 0; i < len; i++)
+        dup[i] = s[i];
+    dup[len] = '\0';
+    return dup;
+}
 
-    cmd->args = malloc(sizeof(char *) * MAX_ARGS);
-    cmd->outfiles = malloc(sizeof(char *) * MAX_REDIRECTS);
-    cmd->append_flags = malloc(sizeof(int) * MAX_REDIRECTS);
+char ***split_by_pipe(char **tokens, int *cmd_count)
+{
+    char ***cmds = malloc(sizeof(char **) * MAX_CMDS);
+    int i = 0, j = 0, k = 0;
 
-    int i = 0, j = 0;
+    cmds[k] = malloc(sizeof(char *) * MAX_TOKENS);
+    if (!cmds[k])
+        return NULL;
     while (tokens[i])
     {
-        if ((strcmp(tokens[i], ">") == 0 || strcmp(tokens[i], ">>") == 0) && tokens[i + 1])
+        if (ft_strcmp(tokens[i], "|") == 0)
         {
-            cmd->outfiles[cmd->out_count] = strdup(tokens[i + 1]);
-            cmd->append_flags[cmd->out_count] = (strcmp(tokens[i], ">>") == 0);
-            cmd->out_count++;
+            cmds[k][j] = NULL;
+            cmds[++k] = malloc(sizeof(char *) * MAX_TOKENS);
+            j = 0;
+        }
+        else
+            cmds[k][j++] = ft_strdup(tokens[i]);
+        i++;
+    }
+    cmds[k][j] = NULL;
+    *cmd_count = k + 1;
+    return cmds;
+}
+
+t_cmd *parse_tokens(char **tokens)
+{
+    t_cmd *cmd = malloc(sizeof(t_cmd));
+    int i = 0, j = 0;
+
+    if (!cmd)
+        return NULL;
+    cmd->cmd = NULL;
+    cmd->args = malloc(sizeof(char *) * MAX_ARGS);
+    cmd->infile = NULL;
+    cmd->outfiles = malloc(sizeof(char *) * MAX_REDIRECTS);
+    cmd->append_flags = malloc(sizeof(int) * MAX_REDIRECTS);
+    cmd->out_count = 0;
+
+    while (tokens[i])
+    {
+        if ((ft_strcmp(tokens[i], ">") == 0 || ft_strcmp(tokens[i], ">>") == 0) && tokens[i + 1])
+        {
+            cmd->outfiles[cmd->out_count] = ft_strdup(tokens[i + 1]);
+            cmd->append_flags[cmd->out_count++] = (tokens[i][1] == '>');
             i += 2;
         }
-        else if (strcmp(tokens[i], "<") == 0 && tokens[i + 1])
+        else if (ft_strcmp(tokens[i], "<") == 0 && tokens[i + 1])
         {
-            cmd->infile = strdup(tokens[i + 1]);
+            cmd->infile = ft_strdup(tokens[i + 1]);
             i += 2;
         }
         else
         {
             if (!cmd->cmd)
-                cmd->cmd = strdup(tokens[i]);
-            cmd->args[j++] = strdup(tokens[i]);
-            i++;
+                cmd->cmd = ft_strdup(tokens[i]);
+            cmd->args[j++] = ft_strdup(tokens[i++]);
         }
     }
-
     cmd->args[j] = NULL;
     return cmd;
 }
 
 void free_cmd(t_cmd *cmd)
 {
+    int i;
+
     if (!cmd)
         return;
-
     if (cmd->cmd)
         free(cmd->cmd);
-
     if (cmd->infile)
         free(cmd->infile);
-
     if (cmd->args)
     {
-        for (int i = 0; cmd->args[i]; i++)
+        for (i = 0; cmd->args[i]; i++)
             free(cmd->args[i]);
         free(cmd->args);
     }
-
-    for (int o = 0; o < cmd->out_count; o++)
-        free(cmd->outfiles[o]);
+    for (i = 0; i < cmd->out_count; i++)
+        free(cmd->outfiles[i]);
     free(cmd->outfiles);
     free(cmd->append_flags);
-
     free(cmd);
 }
--- src/parser/tokenization.c
+++ src/parser/tokenization.c
@@ -15,16 +15,19 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <unistd.h>
 
-static bool is_operator(char c)
+static int ft_strlen(const char *s)
 {
-    return (c == '>' || c == '<' || c == '|');
+    int i = 0;
+    while (s && s[i])
+        i++;
+    return i;
 }
 
 int has_unclosed_quotes(const char *str)
 {
     char quote = 0;
-
     while (*str)
     {
         if (!quote && (*str == '\'' || *str == '"'))
@@ -46,23 +49,30 @@ static char *get_token(const char **str_ptr)
 
     while (*s == ' ')
         s++;
-    if (is_operator(*s))
+    if (*s == '>' || *s == '<')
     {
-        if ((s[0] == '>' && s[1] == '>') || (s[0] == '<' && s[1] == '<'))
-            len = 2;
-        else
-            len = 1;
-
+        len = (s[1] == *s) ? 2 : 1;
         token = malloc(len + 1);
         if (!token)
             return NULL;
-        strncpy(token, s, len);
+        token[0] = *s;
+        if (len == 2) token[1] = *s;
         token[len] = '\0';
         *str_ptr = s + len;
         return token;
     }
+    if (*s == '|')
+    {
+        token = malloc(2);
+        if (!token)
+            return NULL;
+        token[0] = '|';
+        token[1] = '\0';
+        *str_ptr = s + 1;
+        return token;
+    }
     start = s;
-    while (*s && *s != ' ' && !is_operator(*s))
+    while (*s && *s != ' ' && *s != '|' && *s != '<' && *s != '>')
     {
         if (*s == '\'' || *s == '"')
         {
@@ -73,36 +83,35 @@ static char *get_token(const char **str_ptr)
                 s++;
         }
         else
-        {
             s++;
-        }
     }
     len = s - start;
     token = malloc(len + 1);
     if (!token)
         return NULL;
-    strncpy(token, start, len);
+    for (int i = 0; i < len; i++)
+        token[i] = start[i];
     token[len] = '\0';
     *str_ptr = s;
     return token;
 }
+
 char **split_input(char *input)
 {
+    const char *ptr = input;
+    char **tokens;
+    int i = 0;
+
     if (!input)
         return NULL;
     if (has_unclosed_quotes(input))
     {
-        fprintf(stderr, "minishell: syntax error: unclosed quote\n");
+        write(2, "minishell: syntax error: unclosed quote\n", 40);
         return NULL;
     }
-    int max_tokens = strlen(input);
-    char **tokens = malloc(sizeof(char *) * (max_tokens + 1));
+    tokens = malloc(sizeof(char *) * (ft_strlen(input) + 1));
     if (!tokens)
         return NULL;
-
-    const char *ptr = input;
-    int i = 0;
-
     while (*ptr)
     {
         while (*ptr == ' ')
