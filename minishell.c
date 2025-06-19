b32750f02d81a6037bbefed571ee10cbcbfa5d49
saadzaoual <saadzaoual@gmail.com>
2025-06-19 11:30:21 +0100

    syntax error todo: >>a<<

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: szaoual <szaoual@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 18:27:21 by szaoual           #+#    #+#             */
-/*   Updated: 2025/06/18 17:35:42 by szaoual          ###   ########.fr       */
+/*   Updated: 2025/06/19 11:28:35 by szaoual          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,77 @@ const char	*redir_type_str(t_rtype type)
 	if (type == R_OUT)
 		return ("output");
 	return ("append");
+}
+void	free_tokens(char **tokens)
+{
+	int	i;
+
+	if (!tokens)
+		return;
+	i = 0;
+	while (tokens[i])
+	{
+		free(tokens[i]);
+		i++;
+	}
+	free(tokens);
+}
+int	is_operatorsyserror(const char *s)
+{
+	return (!ft_strcmp(s, "|")
+		|| !ft_strcmp(s, ">")
+		|| !ft_strcmp(s, ">>")
+		|| !ft_strcmp(s, "<")
+		|| !ft_strcmp(s, "<<"));
+}
+int	is_redir(const char *s)
+{
+	return (!ft_strcmp(s, ">") || !ft_strcmp(s, ">>")
+		|| !ft_strcmp(s, "<") || !ft_strcmp(s, "<<"));
+}
+
+int	is_pipe(const char *s)
+{
+	return (!ft_strcmp(s, "|"));
+}
+
+int	check_syntax(char **tokens)
+{
+	int i;
+
+	if (!tokens || !tokens[0])
+		return (0);
+
+	i = 0;
+
+	// First token can't be a pipe
+	if (is_pipe(tokens[i]))
+	{
+		printf("minishell: syntax error near unexpected token `%s'\n", tokens[i]);
+		return (1);
+	}
+
+	while (tokens[i])
+	{
+		if (is_pipe(tokens[i]))
+		{
+			if (!tokens[i + 1] || is_pipe(tokens[i + 1]))
+			{
+				printf("minishell: syntax error near unexpected token `%s'\n", tokens[i]);
+				return (1);
+			}
+		}
+		else if (is_redir(tokens[i]))
+		{
+			if (!tokens[i + 1] || is_operatorsyserror(tokens[i + 1]))
+			{
+				printf("minishell: syntax error near unexpected token `%s'\n", tokens[i]);
+				return (1);
+			}
+		}
+		i++;
+	}
+	return (0);
 }
 
 int	main(void)
@@ -44,6 +115,12 @@ int	main(void)
 		{
 			free(input);
 			continue ;
+		}
+		if (check_syntax(tokens))
+		{
+			free_tokens(tokens);
+			free(input);
+			continue;
 		}
 		head = parse_pipeline(tokens);
 		cmd = head;
@@ -72,10 +149,7 @@ int	main(void)
 			i++;
 		}
 		free_cmd_list(head);
-		j = 0;
-		while (tokens[j])
-			free(tokens[j++]);
-		free(tokens);
+		free_tokens(tokens);
 		free(input);
 	}
 	printf("exit\n");
