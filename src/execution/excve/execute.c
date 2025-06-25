36b0a763835890498936678d65b43a8db4aa337c
AelElz <dbsperfect2004@gmail.com>
2025-06-25 21:14:26 +0100

    norm_execute_exevecommands

--- src/execution/excve/execute.c
+++ src/execution/excve/execute.c
@@ -6,7 +6,7 @@
 /*   By: ayoub <ayoub@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 20:19:09 by ayoub             #+#    #+#             */
-/*   Updated: 2025/06/25 20:40:19 by ayoub            ###   ########.fr       */
+/*   Updated: 2025/06/25 21:13:34 by ayoub            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,17 @@
 
 int	exec_external(t_cmd *cmd)
 {
-	pid_t    pid;
-	int     status;
-	char    *path;
+	pid_t	pid;
+	int		status;
+	char	*path;
 
 	path = get_cmd_path(cmd);
 	if (!path)
-	{
-		write (2, cmd->cmd, ft_strlen(cmd->cmd));
-		write (2, ": command not found\n", 20);
-		return (127);
-	}
+		return (write (2, cmd->cmd, ft_strlen(cmd->cmd)),
+		write (2, ": command not found\n", 20), 127);
 	pid = fork();
 	if (pid == -1)
-	{
-		perror("fork");
-		free(path);
-		return (1);
-	}
+		return (perror("fork"), free(path), 1);
 	if (pid == 0)
 	{
 		execve(path, cmd->args, env);
